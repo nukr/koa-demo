abf133e2842812b5ea705fca27f20223ce1ec290
Nukr <nukrs.w@gmail.com>
2015-09-02 15:00:50 +0800

    add middleware

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ import koa from 'koa'
 let port = process.env.PORT || 3000
 let app = koa()
 
+app.use(function * () {
+  this.body = 'if you can see this means server worked'
+})
+
 app.listen(port)
 console.log(`server is listen on port ${port}`)
