8d4442508f4600e2dae024a37287516b4cfa87de
Nukr <nukrs.w@gmail.com>
2015-09-02 11:47:05 +0800

    external port config

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import koa from 'koa'
 
+let port = process.env.PORT || 3000
 let app = koa()
 
-app.listen(3000)
-console.log('server is listen on port 3000')
+app.listen(port)
+console.log(`server is listen on port ${port}`)
